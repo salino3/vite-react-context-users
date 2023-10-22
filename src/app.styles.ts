969b3cfd1d1af83e675173f4a023879b893d1f66
salino3 <programacionflavio@gmail.com>
2023-10-22 12:18:31 +0200

    add gh-pages

--- src/app.styles.ts
+++ src/app.styles.ts
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { theme } from "./themes/theme";
+import { theme } from "./themes";
 
 export const Main = styled.main`
   background-color: ${theme.background.primary};
